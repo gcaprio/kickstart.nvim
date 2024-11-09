b682537091be834969cb7656438a30a8ee1fe990
Griffin Caprio <me@griffincaprio.com>
2024-11-08 22:06:53 -0600

    Enabling custom plugins & adding Copilot plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'github/copilot.vim',
+    cmd = 'Copilot',
+    config = function()
+      -- Copilot configuration if needed
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end,
+  },
+}
