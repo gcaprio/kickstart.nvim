f6f5592f37b7ea9a734eff4c14a58a2f7273d9f6
Griffin Caprio <me@griffincaprio.com>
2025-01-01 23:53:47 -0600

    Adding Copilot configuration. Lazy updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,9 +6,12 @@ return {
   {
     'github/copilot.vim',
     cmd = 'Copilot',
+    lazy = false,
     config = function()
       -- Copilot configuration if needed
       vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ''
       vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
     end,
   },
