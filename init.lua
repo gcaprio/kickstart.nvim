da872ecc2ca2d7baf93b29aa9c59cdda4e9aaab7
Griffin Caprio <me@griffincaprio.com>
2025-03-17 21:17:10 -0500

    Enabling php linter and pyright

--- init.lua
+++ init.lua
@@ -618,10 +618,10 @@ require('lazy').setup({
         rubocop = {},
         ruby_lsp = {},
         html = {},
+        pyright = {},
 
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -715,7 +715,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
+        php = { 'php_cs_fixer', 'pint' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
